dddbabe7763ede865107e09a59f4240b4f8a4f41
Braian Martinez <braian.martz@gmail.com>
2018-04-14 12:00:01 -0300

    Servicio de FireBase listo

--- src/app/services/informacion.service.ts
+++ src/app/services/informacion.service.ts
@@ -7,14 +7,40 @@ export class InformacionService {
 
   info: any = {};
   cargada: boolean = false;
+  cargada_sobre_nosotros = false;
+  equipo:any[] = [];
+
+  urlEquipo:string = "https://paginaweb-116fb.firebaseio.com/equipo.json";
+
 
   constructor( public http: Http ) {
 
+    this.carga_info();
+    this.carga_sobre_nosotros();
+
+  }
+
+
+
+  public carga_info(){
+
     this.http.get("assets/data/info.pagina.json").subscribe( data => {
 
-      console.log( data.json() );
+      // console.log( data.json() );
       this.cargada = true;
       this.info = data.json();
+
+    });
+
+  }
+
+  public carga_sobre_nosotros(){    
+
+    this.http.get( this.urlEquipo ).subscribe( data => {
+
+      console.log( data.json() );
+      this.cargada_sobre_nosotros = true;
+      this.equipo = data.json();
 
     });
 
