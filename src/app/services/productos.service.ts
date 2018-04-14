39c98bf505c1572b482df3b6238ba06263febc2f
Braian Martinez <braian.martz@gmail.com>
2018-04-14 18:57:30 -0300

    Primera versión

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
+import { promise } from 'protractor';
+import { reject } from 'q';
 
 @Injectable()
 export class ProductosService {
@@ -7,6 +9,8 @@ export class ProductosService {
   productos:any[] = [];
   urlIdx:string = "https://paginaweb-116fb.firebaseio.com/productos_idx.json";
   cargando:boolean = true;
+
+  productos_filtrado:any[] = [];
 
   constructor( private http: Http ) {
 
@@ -18,20 +22,26 @@ export class ProductosService {
 
     this.cargando = true;
 
-    this.http.get( this.urlIdx ).subscribe( res => {
+    let promesa = new Promise( ( resolve, reject ) => {
 
-      // console.log( res.json() );
-
-      // setTimeout( () => {
-
-        this.cargando = false;
-        this.productos = res.json();
-
-      // }, 1500);
-
+      this.http.get( this.urlIdx ).subscribe( res => {
+  
+        // console.log( res.json() );
+  
+        // setTimeout( () => {
+  
+          this.cargando = false;
+          this.productos = res.json();
+          resolve();
+  
+        // }, 1500);
+  
+  
+      });
 
     });
 
+    return promesa;
   }
 
 
@@ -41,6 +51,44 @@ export class ProductosService {
 
   }
 
+
+
+
+  public buscar_producto( termino:string ){
+
+    // console.log("Buscando producto");
+    // console.log( this.productos.length );
+
+    if(this.productos.length === 0){
+      this.cargar_productos().then( () => {
+        // termino la carga
+        this.filtrar_productos( termino );
+      });
+    }else{
+      this.filtrar_productos( termino );
+    } 
+
+  }
+
+
+  private filtrar_productos( termino:string ){
+
+    this.productos_filtrado = [];
+
+    termino = termino.toLowerCase();
+
+    this.productos.forEach( prod => {
+
+      if( prod.categoria.indexOf( termino ) >= 0 ||  prod.titulo.toLowerCase().indexOf( termino ) >= 0 ){
+        this.productos_filtrado.push( prod );
+        // console.log(prod);
+      }
+
+      // console.log( prod );
+
+    });
+
+  }
 
 }
 
